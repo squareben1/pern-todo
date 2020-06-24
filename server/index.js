0d3df36359e2f3c01e069d419c6dd6553f054eb9
Squareben1 <bengittins@live.com>
2020-06-25 00:52:33 +0100

    Add and Get todos

--- server/index.js
+++ server/index.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
+
+// routes
+
+// create TODO
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// get all todos
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo")
+    res.json(allTodos.rows)
+  } catch (error) {
+    console.log(error.message);
+  }
+})
+// get a todo
+
+// update a todo
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
