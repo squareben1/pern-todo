2618373f8dd46b1d9ba18452baf1f768cae8d6db
Squareben1 <bengittins@live.com>
2020-06-25 01:05:23 +0100

    Get todo

--- server/index.js
+++ server/index.js
@@ -29,13 +29,26 @@ app.post("/todos", async (req, res) => {
 
 app.get("/todos", async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM todo")
-    res.json(allTodos.rows)
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
   } catch (error) {
     console.log(error.message);
   }
-})
+});
 // get a todo
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 // update a todo
 
