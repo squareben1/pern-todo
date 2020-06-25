1e3cd980cf74ad0a23095189d254a083602f7092
Squareben1 <bengittins@live.com>
2020-06-25 01:16:34 +0100

    Delete todo

--- server/index.js
+++ server/index.js
@@ -52,6 +52,35 @@ app.get("/todos/:id", async (req, res) => {
 
 // update a todo
 
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const udateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was updated");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// delete todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json("Todo was deleted");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
 });
